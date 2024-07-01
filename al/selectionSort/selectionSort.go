09ea5b7d942fcbed981fa092400843c18969c0bf
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2024-07-01 14:19:03 +0300

    add nested loop solution also

--- al/selectionSort/selectionSort.go
+++ al/selectionSort/selectionSort.go
@@ -24,3 +24,18 @@ func SelectionSort(nums []int) {
 
 	}
 }
+
+func SelectionSortNestedLoop(nums []int) {
+	for i := 0; i < len(nums)-1; i++ {
+		minIndex := i
+
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] < nums[minIndex] {
+				minIndex = j
+			}
+		}
+
+		nums[i], nums[minIndex] = nums[minIndex], nums[i]
+
+	}
+}
--- al/selectionSort/selectionSort_test.go
+++ al/selectionSort/selectionSort_test.go
@@ -4,16 +4,56 @@ import "testing"
 
 var(
 	happyPathArray = []int{64, 25, 12, 22, 11}
+	happyPathArrayNested = []int{64, 25, 12, 22, 11}
 )
+
+func areArraysEqual(arr1 []int, arr2 []int) (equal bool, ok bool) {
+	if len(arr1) != len(arr2) {
+		return false, false
+	}
+
+	for i, val := range arr1 {
+		if arr2[i] != val {
+			return false, true
+		}
+	}
+
+	return true, true
+}
 
 func TestHappyPathCase(t *testing.T) {
 	sorted := []int{11,12,22,25,64}
 
 	SelectionSort(happyPathArray)
 
-	for i := 0; i<len(sorted); i++ {
-		if happyPathArray[i] != sorted[i] {
-			t.Fatalf("array could not be sorted properly!!")
-		}
+	equal, ok := areArraysEqual(sorted, happyPathArray)
+
+	if !ok {
+		t.Fatalf("array sizes are not equal!!!")
+	}
+
+	if !equal {
+		t.Fatalf("array could not be sorted properly!!")
 	}
 }
+
+
+func TestHappyPathCaseNested(t *testing.T) {
+
+	sorted := []int{11,12,22,25,64}
+
+	SelectionSortNestedLoop(happyPathArrayNested)
+
+	equal, ok := areArraysEqual(sorted, happyPathArrayNested)
+
+	if !ok {
+		t.Fatalf("array sizes are not equal!!!")
+	}
+
+	if !equal {
+		t.Fatalf("array could not be sorted properly!!")
+	}
+
+}
+
+
