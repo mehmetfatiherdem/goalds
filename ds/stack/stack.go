e1e5b3ebc044551de6a380d4238ee8c9bff8d1a6
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2024-07-14 17:02:05 +0300

    stack done

--- ds/stack/stack.go
+++ ds/stack/stack.go
@@ -2,18 +2,42 @@ package main
 
 import "fmt"
 
-type Node[T interface{}] struct {
+type Node[T comparable] struct {
 	Value T
 	Next  *Node[T]
 }
 
-type Stack[T interface{}] struct {
+type Stack[T comparable] struct {
 	Head *Node[T]
 }
 
-func (s *Stack[T]) Push(n *Node[T]) {
+func (s *Stack[T]) Search(n T) (int, error) {
+	index := 1
+	it := s.Head
+	for it.Value != n {
+		it = it.Next
+		index++
+	}
+
+	if it == nil {
+		panic("value not found in the stack")
+	}
+
+	return index, nil
+} 
+
+func (s *Stack[T]) Empty() bool {
+	return s.Head == nil
+}
+
+func (s *Stack[T]) Peek() (n *Node[T]) {
+	return s.Head
+}
+
+func (s *Stack[T]) Push(val T) {
+	n := Node[T]{Value: val, Next: nil}
 	n.Next = s.Head
-	s.Head = n
+	s.Head = &n
 }
 
 func (s *Stack[T]) Pop() *Node[T] {
@@ -23,16 +47,37 @@ func (s *Stack[T]) Pop() *Node[T] {
 }
 
 func main() {
-	head := Node[int]{Value: 10, Next: nil}
+	stack := Stack[int]{}
 
-	stack := Stack[int]{Head: &head}
+	fmt.Println("stack is empty before:", stack.Empty())
+	
+	stack.Push(10)
+	stack.Push(20)
+	stack.Push(30)
+	stack.Push(40)
+	stack.Push(50)
 
-	stack.Push(&Node[int]{Value: 20, Next: nil})
-	stack.Push(&Node[int]{Value: 30, Next: nil})
-	stack.Push(&Node[int]{Value: 40, Next: nil})
+	fmt.Println("stack is empty after:", stack.Empty())
 
 	for stack.Head != nil {
 		fmt.Println(stack.Head.Value)
 		stack.Pop()
 	}
+
+	fmt.Println("stack is empty after print before search:", stack.Empty())
+
+	stack.Push(10)
+	stack.Push(20)
+	stack.Push(30)
+	stack.Push(40)
+	stack.Push(50)
+	
+	in, err := stack.Search(40)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("searching index of 40:", in)
+	
 }
