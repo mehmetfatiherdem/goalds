a31ea7812f1200b63ad87a0ff59962d3d041f835
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2024-07-15 17:27:49 +0300

    quick sort

---
+++ al/quicksort/quicksort.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func partition(nums []int, low int, high int) int {
+
+	pi := nums[high]
+
+	i := low - 1
+
+	for j := low; j <= high-1; j++ {
+		if nums[j] < pi {
+			i++
+			nums[i], nums[j] = nums[j], nums[i]
+		}
+	}
+
+	i++
+
+	nums[i], nums[high] = nums[high], nums[i]
+	return i
+}
+
+func QuickSort(nums []int, low int, high int) {
+
+	if low < high {
+		pi := partition(nums, low, high)
+
+		QuickSort(nums, low, pi-1)
+		QuickSort(nums, pi+1, high)
+	}
+}
+
+func main() {
+	nums := []int{10, 7, 8, 9, 1, 5}
+
+	QuickSort(nums, 0, len(nums) - 1)
+
+	for _, v := range nums {
+		fmt.Println(v)
+	}
+}
